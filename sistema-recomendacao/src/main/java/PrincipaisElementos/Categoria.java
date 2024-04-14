e252f4c48ffeed63c0083e734aa21199e0d98024
Caio Rocha Fernandes <caiorf2005@gmail.com>
2024-04-14 19:21:13 -0300

    Update Categoria.java

--- sistema-recomendacao/src/main/java/PrincipaisElementos/Categoria.java
+++ sistema-recomendacao/src/main/java/PrincipaisElementos/Categoria.java
@@ -5,6 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Categoria {
@@ -12,6 +19,8 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres")
     @Column(nullable = false)
     private String nome;
 
