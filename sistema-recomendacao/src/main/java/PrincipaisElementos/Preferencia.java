5d69456ff88ad68cb0ca6f604987927a18a1e6ce
Caio Rocha Fernandes <caiorf2005@gmail.com>
2024-04-14 19:14:30 -0300

    Update Preferencia.java

--- sistema-recomendacao/src/main/java/PrincipaisElementos/Preferencia.java
+++ sistema-recomendacao/src/main/java/PrincipaisElementos/Preferencia.java
@@ -7,6 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Preferencia {
@@ -14,11 +24,15 @@ public class Preferencia {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres")
     @Column(nullable = false)
     private String nome;
     
+    @Size(max = 100, message = "A descrição deve ter no máximo 100 caracteres")
     private String descricao;
     
+    @NotNull(message = "O usuário é obrigatório")
     @ManyToOne
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
