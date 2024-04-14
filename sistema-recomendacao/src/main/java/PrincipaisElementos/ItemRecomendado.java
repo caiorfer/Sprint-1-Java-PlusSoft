433a9b496aa8c4f9125a72f22db9efe5746e3868
Caio Rocha Fernandes <caiorf2005@gmail.com>
2024-04-14 19:16:38 -0300

    Update ItemRecomendado.java

--- sistema-recomendacao/src/main/java/PrincipaisElementos/ItemRecomendado.java
+++ sistema-recomendacao/src/main/java/PrincipaisElementos/ItemRecomendado.java
@@ -7,6 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class ItemRecomendado {
@@ -14,14 +24,17 @@ public class ItemRecomendado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 50, message = "O nome deve ter no máximo 50 caracteres")
     @Column(nullable = false)
     private String nome;
-    
+
     private String descricao;
     
     @ManyToOne
     @JoinColumn(name = "categoria_id", nullable = false)
     private Categoria categoria;
+
 
 	public Long getId() {
 		return id;
